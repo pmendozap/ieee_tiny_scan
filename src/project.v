59a9c4156e7602110d4585385d560b2ee3f1822c
Pablo Mendoza <pmendoza@itcr.ac.cr>
2026-05-08 12:43:01 -0600

    new module

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_pmendoza_ieee_tinyscan (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,36 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+
+
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in[3:0], 1'b0};
+  
+  
+    uni_reg u0 (
+    .clk(clk),
+    .reset(rst_n),
+    .control(uio_in[7:4]),
+    .data_i(ui_in[7:4]),
+    .data_o(uo_out[3:0]),
+    .SI(ui_in[0]), .SE(ui_in[1]), .SO(uio_out[0])
+  );
+  
+  
+  uni_reg u1 (
+    .clk(clk),
+    .reset(rst_n),
+    .control(uio_in[7:4]),
+    .data_i(ui_in[7:4]),
+    .data_o(uo_out[7:4]),
+    .SI(ui_in[2]), .SE(ui_in[3]), .SO(uio_out[1])
+  );
+  
+    
+  assign uio_oe  = 8'b00000011;
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uio_out[7:2] = 0;
+  
 
 endmodule
